d6deb3b7204e0a352204824830ac3ef032ca3638
crismaster <crismaster1000@hotmail.com>
2020-12-12 18:34:05 -0500

    starbucks page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function imgSlider(imagen){
+    document.querySelector('.starbucks').src = imagen;
+}
+
+function fondo(color){
+    const circulo = document.querySelector('.circulo');
+    circulo.style.background = color;
+}
